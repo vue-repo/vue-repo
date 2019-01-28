7d8ada435cc23ec88df129f122abfcadc21d3885
Roman Rodionov <rrodionov91@mail.ru>
2019-01-28 09:04:17 +0300

    Add index file

---
+++ index.ts
@@ -0,0 +1,10 @@
+import {StoreModule} from 'src/StoreModule/StoreModule'
+import {AbstractCRUDRepository as AbstractCRUDRepo} from 'src/base_repositories/AbstractCRUDRepository'
+
+export {
+    StoreModule,
+    AbstractCRUDRepo
+}
+
+export * from 'src/StoreModule/middlewares'
+export * from 'src/StoreModule/annotations'
